0bd8cadc0e19577b0bff44e809fd597de29ded9a
Mionne Gooch <mionne.gooch@outlook.com>
2021-08-30 16:51:56 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from tkinter import *
+
+root = Tk()
+
+
+# create function for button
+def myClick():
+    myLabel = Label(root, text="Look! i clicked button")
+    myLabel.pack()
+
+
+# create label widget
+# myLabel1 = Label(root, text="Hello World")
+# myLabel2 = Label(root, text="Hi, My name is, What")
+myButton = Button(root, text="Click Me", command = myClick)
+# shoving it onto screen
+# myLabel1.grid(row=0, column=0)
+# myLabel2.grid(row=1, column=5)
+myButton.pack()
+
+root.mainloop()
