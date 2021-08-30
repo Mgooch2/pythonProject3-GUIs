f4602c5e093892ab2098442d228161575b1f72fb
Mionne Gooch <mionne.gooch@outlook.com>
2021-08-30 16:56:23 -0400

    after buttons tutorial

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def myClick():
 # create label widget
 # myLabel1 = Label(root, text="Hello World")
 # myLabel2 = Label(root, text="Hi, My name is, What")
-myButton = Button(root, text="Click Me", command = myClick)
+myButton = Button(root, text="Click Me", command = myClick, fg = "red", bg ="black")#don't add parenthesis on this function or else it won't work right
 # shoving it onto screen
 # myLabel1.grid(row=0, column=0)
 # myLabel2.grid(row=1, column=5)
