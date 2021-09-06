56ea4e783bca4be52ff6b237e4bdca97c06e6505
Mgooch2 <86744580+Mgooch2@users.noreply.github.com>
2021-09-06 13:54:32 -0400

    finished up with databases learning how to update, began weather app video

--- databases.py
+++ databases.py
@@ -25,6 +25,126 @@ c = conn.cursor()
 #         zipcode integer
 #         )""")
 
+#update function
+def update():
+     # create a database or connect to one
+    conn = sqlite3.connect('address_book.db')
+    # create a cursor
+    c = conn.cursor()
+    record_id = delete_box.get()
+    c.execute("""UPDATE addresses SET
+        first_name = :first,
+        last_name = :last,
+        address = :address,
+        city = :city,
+        state = :state,
+        zipcode = :zipcode
+
+        WHERE oid = :oid""",
+        {
+        'first':f_name_editor.get(),
+        'last':l_name_editor.get(),
+        'address':address_editor.get(),
+        'city': city_editor.get(),
+        'state': state_editor.get(),
+        'zipcode': zipcode_editor.get(),
+
+        'oid': record_id
+        })
+
+    # commit changes to database
+    conn.commit()
+    # close connection
+    conn.close()
+
+    editor.destroy()
+
+#create edit function
+def edit():
+    global editor
+    editor = Tk()
+    editor.title("Update a record")
+    editor.geometry("400x400")
+
+    # create a database or connect to one
+    conn = sqlite3.connect('address_book.db')
+    # create a cursor
+    c = conn.cursor()
+
+    record_id = delete_box.get()
+    # query the database
+    c.execute("SELECT * FROM addresses WHERE oid = " + record_id)
+    records = c.fetchall()
+
+    #create globals for text box names
+    global f_name_editor
+    global l_name_editor
+    global address_editor
+    global city_editor
+    global state_editor
+    global zipcode_editor
+
+
+  # create text boxes
+    f_name_editor = Entry(editor, width=30)
+    f_name_editor.grid(row=0, column=1, padx=20,pady=(10,0))
+
+    l_name_editor = Entry(editor, width=30)
+    l_name_editor.grid(row=1, column=1)
+
+    address_editor = Entry(editor, width=30)
+    address_editor.grid(row=2, column=1)
+
+    city_editor = Entry(editor, width=30)
+    city_editor.grid(row=3, column=1)
+
+    state_editor = Entry(editor, width=30)
+    state_editor.grid(row=4, column=1)
+
+    zipcode_editor = Entry(editor, width=30)
+    zipcode_editor.grid(row=5, column=1)
+
+
+
+    # create text box labels
+    f_name_label = Label(editor, text="First Name")
+    f_name_label.grid(row=0, column=0, pady=(10,0))
+
+    l_name_label = Label(editor, text="Last Name")
+    l_name_label.grid(row=1, column=0)
+
+    address_label = Label(editor, text="Address")
+    address_label.grid(row=2, column=0)
+
+    city_label = Label(editor, text="City")
+    city_label.grid(row=3, column=0)
+
+    state_label = Label(editor, text="State")
+    state_label.grid(row=4, column=0)
+
+    zipcode_label = Label(editor, text="Zipcode")
+    zipcode_label.grid(row=5, column=0)
+
+
+    #Loop through results
+    for record in records:
+        f_name_editor.insert(0,record[0])
+        l_name_editor.insert(0,record[1])
+        address_editor.insert(0,record[2])
+        city_editor.insert(0,record[3])
+        state_editor.insert(0,record[4])
+        zipcode_editor.insert(0,record[5])
+
+
+    #create a save button to save edited record
+    save_btn = Button(editor, text="Save Record", command=update)
+    save_btn.grid(row=6, column=0, columnspan=2, pady=10, padx=10, ipadx=145)
+
+
+
+
+
+
 #create function to delete a record
 def delete():
     # create a database or connect to one
@@ -89,7 +209,7 @@ def query():
         print_records += str(record[0]) + " " + str(record[1]) +" "+"\t"+str(record[6])+ "\n"
 
     query_label = Label(root, text=print_records)
-    query_label.grid(row=11, column=0, columnspan=2)
+    query_label.grid(row=12, column=0, columnspan=2)
 
     # commit changes to database
     conn.commit()
@@ -138,7 +258,7 @@ state_label.grid(row=4, column=0)
 zipcode_label = Label(root, text="Zipcode")
 zipcode_label.grid(row=5, column=0)
 
-delete_box_label = Label(root,text="Delete ID")
+delete_box_label = Label(root,text="Select ID")
 delete_box_label.grid(row=9, column=0, pady=5)
 
 
@@ -157,7 +277,9 @@ conn.commit()
 delete_btn = Button(root, text="Delete Record", command=delete)
 delete_btn.grid(row=10, column=0, columnspan=2, pady=10, padx=10, ipadx=136)
 
-
+#create an update button
+edit_btn = Button(root, text="Edit Record", command=edit)
+edit_btn.grid(row=11, column=0, columnspan=2, pady=10, padx=10, ipadx=143)
 # close connection
 conn.close()
 
---
+++ weatherapp.py
@@ -0,0 +1,13 @@
+from tkinter import *
+from PIL import ImageTk,Image
+
+root = Tk()
+root.title('Weather App')
+root.geometry('400x400')
+
+
+
+
+
+
+root.mainloop()
